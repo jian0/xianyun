8bbfdfb18373919f91e2836ba309fe30b2651c99
jianren <365088870@qq.com>
2020-01-16 18:24:24 +0800

    03-公共样式页头+页尾完成

---
+++ components/header.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-row type="flex" justify="space-between" class="header" style="margin:auto">
+      <div class="img"></div>
+      <div class="navs">
+          <el-row type="flex">
+              <nuxt-link to="/">首页</nuxt-link>
+              <nuxt-link to="/post">旅游攻略</nuxt-link>
+              <nuxt-link to="/hotel">酒店</nuxt-link>
+              <nuxt-link to="/air">国内机票</nuxt-link>
+          </el-row>
+      </div>
+      <nuxt-link class="login" to="/user/login">登录/注册</nuxt-link>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.header {
+    width: 1000px;
+    min-width: 1000px;
+    // border-bottom: 2px solid #ccc;
+    box-shadow: 0 3px 2px #f6f6f6
+}
+.img {
+    display: block;
+    width: 156px;
+    height: 60px;   
+    background: url('/logo.jpg') center no-repeat;
+    background-size: 100%;
+}
+.navs {
+    flex: 1;
+    margin: 0 20px;
+    a {
+        display:block;
+        padding:0 20px;
+        height:60px;
+        box-sizing: border-box;
+        line-height: 60px;
+    }
+}
+.login {
+    height: 60px;
+    line-height: 60px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
+    <Header></Header>
     <nuxt />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/header.vue'
+import Footer from '@/components/footer.vue'
 export default {
-  
+    data () {
+      return {
+        
+      }
+    },
+    components:{
+      Header,Footer
+    }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 
+ul,ol,li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: #000
+}
+
+em,i {
+  font-style: normal;
+}
 </style>
